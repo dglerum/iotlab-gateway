cb65352097d9479e3a879520ea6823af5d0974a2
Davy Glerum <dglerum@Davys-MacBook-Pro.local>
2018-09-24 21:31:22 +0200

    added hass on port 80

--- nodered/app/settings.js
+++ nodered/app/settings.js
@@ -103,15 +103,12 @@ module.exports = {
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
     adminAuth: {
-       type: "credentials",
-       users: [{
-           username: "admin",
-           password: "secret",
-           permissions: "*"
-       }],
-       // default: {
-       //   permissions: "read"
-       // }
+      type: "credentials",
+      users: [{
+          username: "admin",
+          password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
+          permissions: "*"
+      }]
     },
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
